8d526e00068047e7519bf1e3a2476510a8e62636
Tahmid Khan <77325077+tahmidkhanshuvo@users.noreply.github.com>
2025-08-15 19:37:36 +0600

    Just for test purpose

---
+++ hello.js
@@ -0,0 +1,74 @@
+do\onabort() {
+  console.log("Abort event triggered");
+}
+
+async mmsain() {
+  console.log("Main function started");
+  // Simulate some asynchronous operation
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  console.log("We shoukd be able to see this message after 1 second");
+    onabort();
+    console.log("Main function completed");
+}
+
+NamedNodeMap mmsain();
+mmsain().catch(error => console.error("Error in main function:", error));
+KeyboardEvent
+matchMedia jjwq
+function onabort() {
+  console.log("Abort event triggered");
+}
+
+matchMedia("screen and (max-width: 600px)").addEventListener("change", event => {
+  if (event.matches) {
+    console.log("Screen width is less than or equal to 600px");
+  } else {
+    console.log("Screen width is greater than 600px");
+  }
+});
+
+function onabort() {
+  console.log("Abort event triggered");
+}
+
+function onabort() {
+    console.log("Abort event triggered");
+    console.log("Abort event triggered");
+
+    console.log("Abort event triggered");
+    console.log("Abort event triggered");
+    console.log("Abort event triggered");
+
+
+
+    StrictMode,MediaSession, satisfies
+    webkitURLw
+function onabort() {
+  console.log("Abort event triggered");
+    console.log("Abort event triggered");
+    console.log("Abort event triggered");
+    console.log("Abort event triggered");
+
+    console.log("Abort event triggered");
+
+    console.log("Abort event triggered");
+    console.log("Abort event triggered");
+
+    console.log("Abort event triggered");
+    console.log("Abort event triggered");
+
+    console.log("Abort event triggered");
+    console.log("Abort event triggered");
+}
+
+function onabort() {
+mmms
+SVGFEDropShadowElement
+matchMedia("screen and (max-width: 600px)").addEventListener("change", event => {
+  if (event.matches) {
+    console.log("Screen width is less than or equal to 600px");
+  } else {
+    console.log("Screen width is greater than 600px");
+  }
+}
+);mmms
