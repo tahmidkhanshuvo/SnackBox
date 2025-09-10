dec48d2ca2f76c332594c1ac4b2262ac1abadc9c
Saobia3i <islamsaobia@gmail.com>
2025-09-11 00:46:58 +0600

    deleted Customer Home page

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="p-4 bg-red-700 text-white text-center">
+      &copy; 2025 SnackBox. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
